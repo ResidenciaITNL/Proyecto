388f5bfb387d39c53240acd0a68d6becbdcf07b4
Alberto Barrientos Davila <37272848+ABD2k@users.noreply.github.com>
2024-05-16 15:05:49 -0600

    cambiar mi password

--- Sistema/Controllers/MyAccountController.cs
+++ Sistema/Controllers/MyAccountController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sistema.DataBase;
 using Sistema.Models;
+using System.Security.Claims;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,21 +23,31 @@ namespace Sistema.Controllers
         }
 
         // PUT api/<MyAccountController>/5
-        [HttpPut]
-        public async Task<IActionResult> Put([FromBody] Users value)
+        [HttpPut("change-password")]
+        public async Task<IActionResult> Put([FromBody] UsersUpdate value)
         {
-            Users? user = await _context.Users.FirstOrDefaultAsync(x => x.UserId == CustomerId);
+            int UserId = int.Parse(User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value);
+            Users? user = await _context.Users.FirstOrDefaultAsync(x => x.UserId == UserId);
             if (user == null)
             {
                 return NotFound(new { message = "Usuario no encontrado" });
             }
             if (value.password != null)
             {
+                if (value.password.Length < 8)
+                {
+                    return BadRequest(new { message = "La contraseña debe tener al menos 8 caracteres" });
+                }
+                if (!user.CheckPassword(value.oldPassword))
+                {
+                    return BadRequest(new { message = "Contraseña incorrecta" });
+                }
                 user.password = value.password;
                 user.HashPassword();
             }
+            _context.Entry(user).State = EntityState.Modified;
             await _context.SaveChangesAsync();
-            return Ok(new { message = "Usuario actualizado" });
+            return Ok(new { message = "La contraseña se actualizo con exito" });
         }
     }
 }
